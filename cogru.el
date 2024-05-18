7133a8157c59f1c306cca5c49639194806cf2de9
JenChieh <jcs090218@gmail.com>
2024-05-18 15:07:03 -0700

    Add connection

--- cogru.el
+++ cogru.el
@@ -37,11 +37,58 @@
   :group 'tool
   :link '(url-link :tag "Repository" "https://github.com/Cogru/cogru.el"))
 
+(defcustom cogru-host "localhost"
+  "Default host to connect to the server."
+  :type 'string
+  :group 'cogru)
+
+(defcustom cogru-port 8786
+  "Default port to connect to the server."
+  :type 'integer
+  :group 'cogru)
+
+(defvar cogru-process nil
+  "Process to one workspace.")
+
+(defun cogru-address ()
+  "Return the address name."
+  (format "http://%s:%s" cogru-host cogru-port))
+
+(defun cogru--filter (proc data &rest _)
+  "Process DATA from PROC."
+  ;; TODO: ..
+  )
+
 ;;;###autoload
 (defun cogru-start ()
   "Start from connecting to the server."
   (interactive)
-  (let ))
+  (cond
+   (cogru-process
+    (user-error "[WARNING] The connection is already established; only one server-client connection is allowed"))
+   (t
+    (let* ((default-addr (cogru-address))
+           (addr (read-string "Host url: " default-addr))
+           (url-info (url-generic-parse-url addr))
+           (host (url-host url-info))
+           (port (url-port url-info)))
+      (message "[INFO] Connecting to %s..." addr)
+      (setq cogru-process
+            (make-network-process :name "*tcp-server-cogru*"
+                                  :buffer "*tcp-server-cogru*"
+                                  :filter #'cogru--filter
+                                  :host host
+                                  :service port))
+      (message "[INFO] Connected to %s" cogru-process)))))
+
+(defun cogru-stop ()
+  "Stop the connection."
+  (interactive)
+  (cond (cogru-process
+         (delete-process cogru-process)
+         (setq cogru-process nil)
+         (message "[INFO] Safely disconnected from the server"))
+        (t (user-error "[WARNING] No connection is established; this does nothing"))))
 
 (provide 'cogru)
 ;;; cogru.el ends here
