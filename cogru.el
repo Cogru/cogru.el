9db268aedf0225b96af5748d9e9da13b033e6858
JenChieh <jcs090218@gmail.com>
2024-06-04 18:10:08 -0700

    feat: Add tip

--- cogru.el
+++ cogru.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; URL: https://github.com/Cogru/cogru.el
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "26.1") (msgu "0.1.0") (s "1.12.0") (ht "2.0") (log4e "0.4.1"))
+;; Package-Requires: ((emacs "26.1") (msgu "0.1.0") (s "1.12.0") (ht "2.0") (log4e "0.4.1") (posframe "1.4.3") (popup "0.5.3"))
 ;; Keywords: convenience cogru
 
 ;; This file is not part of GNU Emacs.
